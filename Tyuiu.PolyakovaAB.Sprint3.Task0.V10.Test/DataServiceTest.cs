e0137ca8e2d0ab6992e1c4588b3262c14b4a3913
PolyakovaAB <Tyuiu.PolyakovaA@outlook.com>
2026-09-21 23:03:47 +0500

    Add project files.

---
+++ Tyuiu.PolyakovaAB.Sprint3.Task0.V10.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.PolyakovaAB.Sprint3.Task0.V10.Lib;
+namespace Tyuiu.PolyakovaAB.Sprint3.Task0.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int a = 5;
+            int b = 1;
+            int c = 5;
+            Assert.AreEqual(0.033264872534185766, ds.GetMultiplySeries(a, b, c));
+        }
+    }
+}
---
+++ Tyuiu.PolyakovaAB.Sprint3.Task0.V10/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.PolyakovaAB.Sprint3.Task0.V10.Lib;
+namespace Tyuiu.PolyakovaAB.Sprint3.Task0.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x, b, c;
+            Console.Title = "Спринт #3 | Выполнила: Полякова А. В. | ИИПб-26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнила: Полякова Арина Вячеславовна | ИИПб-26-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет произведение   *");
+            Console.WriteLine("* ряда по формуле.                                                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Введите x:                                                              *");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("* Введите start i:                                                        *");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("* Введите stop i:                                                         *");
+            c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine(ds.GetMultiplySeries(x, b, c));
+            Console.ReadKey();
+        }
+    }
+}
